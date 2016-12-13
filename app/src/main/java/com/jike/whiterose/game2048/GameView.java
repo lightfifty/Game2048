953b2ee5f906f2f72e62326e60653800bc0dafa0
lightfifty <838640006@qq.com>
2016-12-13 22:28:58 +0800

    完成了基础页面布局，完成了方式监听算法

--- app/src/main/java/com/jike/whiterose/game2048/GameView.java
+++ app/src/main/java/com/jike/whiterose/game2048/GameView.java
@@ -10,7 +10,7 @@ import android.widget.GridView;
 /**
  * Created by whiterose on 2016/12/13.
  * 这个是游戏的主类，这里我们需要得到xml里面的参数，所以我们得有相关的构造方法，
- * */
+ */
 
 public class GameView extends GridView {
 
@@ -34,34 +34,34 @@ public class GameView extends GridView {
      * 上面三个构造方法，不论从哪个方法进入，都调用initGameView方法，
      * 相当于initGameview方法是我们这个类的入口方法。
      */
-    private void initGameView(){
+    private void initGameView() {
         setOnTouchListener(new OnTouchListener() {
-            private float startX,startY,offsetX,offsetY;
+            private float startX, startY, offsetX, offsetY;
+
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch (motionEvent.getAction()){
+                switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        Log.d("dd","ddd");
-                        startX=motionEvent.getX();
-                        startY=motionEvent.getY();
+//                        Log.d("dd","ddd");
+                        startX = motionEvent.getX();
+                        startY = motionEvent.getY();
                         break;
                     case MotionEvent.ACTION_UP:
-                        offsetX=motionEvent.getX()-startX;
-                        offsetY=motionEvent.getY()-startY;
-                        Log.d("aa","aaa"+offsetX);
-                        Log.d("aa","aaa"+offsetY);
-                        if (Math.abs(offsetX)>Math.abs(offsetY)){
-                            if (offsetX<-5){
-                                System.out.print("left");
-
-                            }else if (offsetX>5){
-                                System.out.print("right");
+                        offsetX = motionEvent.getX() - startX;
+                        offsetY = motionEvent.getY() - startY;
+//                        Log.d("aa","aaa"+offsetX);
+//                        Log.d("aa","aaa"+offsetY);
+                        if (Math.abs(offsetX) > Math.abs(offsetY)) {
+                            if (offsetX < -5) {
+                                Log.d("left", "left");
+                            } else if (offsetX > 5) {
+                                Log.d("right", "right");
                             }
-                        }else {
-                            if (offsetY<-5){
-                                System.out.print("up");
-                            }else if (offsetY>5){
-                                System.out.print("down");
+                        } else {
+                            if (offsetY < -5) {
+                                Log.d("up", "up");
+                            } else if (offsetY > 5) {
+                                Log.d("down", "down");
                             }
                         }
                         break;
@@ -70,6 +70,22 @@ public class GameView extends GridView {
                 return true;
             }
         });
+
+    }
+
+    private void swipeLeft() {
+
+    }
+
+    private void swipeRight() {
+
+    }
+
+    private void swipeUp() {
+
+    }
+
+    private void swipeDown() {
 
     }
 }
